115ca006f743e8a73c7abc1a6c1913c5291a7c8c
Abheeta A <abheeta09@gmail.com>
2022-03-28 14:39:31 +0530

    flex-first

---
+++ angular-flex/src/app/flex/flex.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flex',
+  templateUrl: './flex.component.html',
+  styleUrls: ['./flex.component.css']
+})
+export class FlexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isMobile() {
+    return true;
+  }
+
+  invisibleOnDesktop() {
+    return false;
+  }
+
+}
